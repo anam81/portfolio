8e900faa439742bcdfea0c1433cf562a4e0bf2da
anam81 <121813494+anam81@users.noreply.github.com>
2026-03-16 23:07:18 +0100

    Add files via upload

--- videos_client.js
+++ videos_client.js
@@ -1,62 +1,61 @@
 const videos = [
+    {
+        id: "221156939",
+        title: "Spotify Hashtag-Generator",
+        description: "I animated this video teasing Spotify´s Hashtag-Generator. Design by " +
+            "<a href='https://www.dayy.com/' target='_blank'>dayy</a>"
+    },
     { 
         id: "221156673", 
         title: "Starkregen", 
-        description: "Graphic animation for science TV program "Quarks & Co", WDR"
+        description: "Graphic animation for science TV program 'Quarks & Co', WDR"
     },
     { 
-        id: "221156939", 
-        title: "Spotify Hashtag-Generator", 
-        description: "I animated this video teasing Spotify´s Hashtag-Generator. Design by" +
-                        "<a href='https://www.dayy.com/' target='_blank'>dayy</a>"
+        id: "222078676", 
+        title: "westart studio background", 
+        description: "I delevoped many of the screenbackgrounds of the WDR Westart live studio. These are some of those."
     },
     { 
-        id: "159606491", 
-        title: "impulses", 
-        description: "A small project I did some time ago, but was forgotten then. I thought its worth unwrapping it because I like flourishing things."
+        id: "221081352", 
+        title: "Opener ZDF Adieu", 
+        description: "I animated this opener for the tv show ZDF Adieu.<br>"+
+                    "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>" +
+                    "music: The Fall by Peter Rudenko licensed under CC BY 4.0"
     },
     { 
-        id: "259777046", 
-        title: "the tri drank the angle", 
-        description: "The tri drank the angle - exploring and playing around with triangles and hexagons"
+        id: "763383592", 
+        title: "Gesundes Sperma", 
+        description: "Animation for science tv program Quarks, WDR"
     },
     { 
-        id: "363118107", 
-        title: "circulation", 
-        description: "An endless circulation"
+        id: "760875033", 
+        title: "Warum werden Tiere im Zoo getötet", 
+        description: "Animation for science tv program Quarks, WDR"
     },
     { 
-        id: "141259204", 
-        title: "der weg ist das ziel", 
-        description: 'A little animation I made circling around with the old saying "The journey is the reward". Sound by ' + 
-            "<a href='https://www.instagram.com/m.zilz/?hl=de' target='_blank'>Marcus Zilz</a>"
+        id: "760873301", 
+        title: "7Dinge_Stimme", 
+        description: "Animation for science tv program Quarks, WDR"
     },
     { 
-        id: "760855186", 
-        title: "pbm.radio", 
-        description: "I animated this opener for the PhotoBookMuseum Let´s talk sessions. Original graphics designed by " +
-                    "<a href='https://jorgealderete.com/' target='_blank'>Dr. Alderete</a>"
+        id: "260259258", 
+        title: "multiresistente Keime", 
+        description: "Animation for science tv program Quarks, WDR"
     },
     { 
-        id: "6798661", 
-        title: "aviarium", 
-        description: "Aviarium, animated short - this is a surreal, dreamlike journey through apocalyptical landscapes."
-    },
-    { 
-        id: "13058458", 
-        title: "Floating Point", 
-        description: "Floating Point, experimental short - Natural phenomenona such as wind, water, clouds or smoke has been recorded on video. Various technics extract their movements, " +
-        "convert them to numbers and write them into files. These are used to control the behaviour and the sound of artificial objects."
+        id: "90454734", 
+        title: "ARD Radio Feature", 
+        description: 'I animated this TV Trailer for "Das ARD Radio Feature"'
     },
     {
-        id: "115930638",
-        title: "algorhythmus",
-        description: "algorhythmus - I was experimenting with mathematically controlled copys of layers forming a logo."
+        id: "50453842",
+        title: "Counter IAA 2011",
+        description: "I designed and animated this counter for a car presentation at the IAA 2011."
     },
     { 
-        id: "60821187", 
-        title: "ejercicio nocturno", 
-        description: "a kinetic type animation experiment"
+        id: "74857016", 
+        title: "CityLeaks Documentary Titles", 
+        description: "Titles/Graphics for City Leaks Cologne Festival Documentary by Ömer Mutlu"
     }
 
 ]
